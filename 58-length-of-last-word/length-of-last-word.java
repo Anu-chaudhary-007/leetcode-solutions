dcd877ae081e8aee71a889a72e641d2ddcda1ede
Anu chaudhary <anuchaudhary23107@gmail.com>
2026-08-30 15:27:03 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (32.44%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int n=s.length();
+        int ans=0;
+        int i=n-1;
+        while(i>=0 && s.charAt(i)==' '){i--;}
+        while(i>=0 && s.charAt(i)!=' '){
+            ans++;
+            i--;
+        }
+        return ans;
+        
+    }
+}
